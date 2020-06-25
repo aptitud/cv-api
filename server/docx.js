cd5d8e10bea32a86fe233640527bb0b231920d95
rdngr <r@dngr.se>
2020-06-25 10:35:17 +0200

    docx: Add intro paragraphs and initial styling

--- server/docx.js
+++ server/docx.js
@@ -1,14 +1,41 @@
 const { Document, Paragraph, TextRun, HeadingLevel, Packer } = require('docx')
 
 module.exports = async cv => {
-  const doc = new Document()
+  const doc = new Document({
+    styles,
+  })
+  const introductionParagraphs = cv.introduction
+    .split('\n')
+    .filter(x => Boolean(x.trim()))
   doc.addSection({
     children: [
       new Paragraph({
         children: [new TextRun(cv.name)],
         heading: HeadingLevel.HEADING_1,
       }),
+      ...introductionParagraphs.map(
+        x =>
+          new Paragraph({
+            children: [new TextRun(x)],
+          }),
+      ),
     ],
   })
   return Packer.toBuffer(doc).then(x => Buffer.from(x))
+}
+
+const styles = {
+  paragraphStyles: [
+    {
+      id: 'Normal',
+      name: 'Normal',
+      quickFormat: true,
+      paragraph: {
+        spacing: {
+          after: 200,
+          line: 276,
+        },
+      },
+    },
+  ],
 }
